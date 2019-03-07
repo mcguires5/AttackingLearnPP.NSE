f3639a1e74248ee24b5f363907813eaf46666768
Delengowski_Mobile <delengowm1@students.rowan.edu>
2019-03-07 12:50:54 -0500

    push for sean

--- attackingLearnPlusPlus_NSE.m
+++ attackingLearnPlusPlus_NSE.m
@@ -22,46 +22,43 @@ net.threshold = 0.01;         % how small is too small for error
 net.mclass = 2;               % number of classes in the prediciton problem
 net.base_classifier = model;  % set the base classifier in the net struct
 
+classNumber = 1; % class to attack and build SINDy models for
 
 
+% These cell arrays are structured as {1,timeStep}
+numTimeSteps = length(data_train);
+[numObs numDims] = size(data_train{1});
+classLabelsIdxs = cell(1,numTimeSteps);
+classData = cell(1,numTimeSteps);
+classMeans = cell(1,numTimeSteps);
+classVariances = cell(1,numTimeSteps);
+classLabelsIdxs = cellfun(@(x) eq(x,classNumber),labels_train,'UniformOutput',false); % Get indexes for data points of classNumber at each time step
+classData = cellfun(@(a,b) a(b,:),data_train,classLabelsIdxs,'UniformOutput',false); % Now extract the class data using logical indexing at each time step 
+classMeans = cellfun(@(x) mean(x),classData,'UniformOutput',false); % Calculate the means at every time step
+classVariances = cellfun(@(x) cov(x),classData,'UniformOutput',false); % Calculate the covariance at every time step
 
-% These cell arrays are structured as {class}{timeStep}
-numClasses = numel(unique(labels_train{1}));
-classLabelsIdxs = cell(1,numClasses);
-classData = cell(1,numClasses);
-classMeans = cell(1,numClasses);
-classVariances = cell(1,numClasses);
-for iClass = 1:numClasses
-	classLabelsIdxs{iClass} = cellfun(@(x) eq(x,1),labels_train,'UniformOutput',false);
-	classData{iClass} = cellfun(@(a,b) a(b,:),data_train,classLabelsIdxs{iClass},'UniformOutput',false);
-	classMeans{iClass} = cellfun(@(x) mean(x),classData{iClass},'UniformOutput',false);
-	classVariances{iClass} = cellfun(@(x) cov(x),classData{iClass},'UniformOutput',false);
-end
-
-% Create a SINDy model and means, 1 by number classes SINDy object array
-numDims = size(data_train{1},2);
-sindyMeans(1,numClasses) = SINDy(); % Object array of sindy objects
-[sindyMeans.lambda] = deal(2e-6);
-[sindyMeans.polyOrder] = deal(5);
-[sindyMeans.useSine] = deal(0);
-[sindyMeans.sineMultiplier] = deal(10);
-[sindyMeans.useExp] = deal(0);
-[sindyMeans.expMultiplier] = deal(10);
-[sindyMeans.useCustomPoolData] = deal(1);
-[sindyMeans.nonDynamical] = deal(0);
-% TVRegDiff parameters for sindy
-[sindyMeans.useTVRegDiff] = deal(0);
-[sindyMeans.iter] = deal(sindy.iter;) 
-[sindyMeans.alph] = deal(sindy.alph);
-[sindyMeans.ep] = deal(sindy.ep);
-[sindyMeans.scale] = deal(sindy.scale);
-[sindyMeans.plotflag] = deal(0);
-[sindyMeans.diagflag] = deal(0);
-% Repeat for covariances this is accessed (row,column) = size of covariance matrix, and then the depth is classes
-% So for 2 dim data, covariance matrix is 2x2, and if there's two classes then sindyCovariances(1,1,1) accesses
-% the sindy object for class 1, row 1 column 1 of its covariance matrix.
-[heightCovMat widthCovMat] = size(classVariances{1}{1});
-sindyCovariances(heightCovMat,widthCovMat,numClasses) = SINDy();
+% Create a SINDy model for means, 1 Obj that will be continuously updated.
+sindyMeans = SINDy(); % SINDy Object ... type help SINDy for list of what each parameter does
+sindyMeans.lambda = 2e-6;
+sindyMeans.polyOrder = 5;
+sindyMeans.useSine = 0;
+sindyMeans.sineMultiplier = 10;
+sindyMeans.useExp = 0;
+sindyMeans.expMultiplier = 10;
+sindyMeans.useCustomPoolData = 1;
+sindyMeans.nonDynamical = 0;
+% TVRegDiff parameters for SINDy 
+sindyMeans.useTVRegDiff = 0;
+sindyMeans.iter = sindyMeans.iter; 
+sindyMeans.alph = sindyMeans.alph;
+sindyMeans.ep = sindyMeans.ep;
+sindyMeans.scale = sindyMeans.scale;
+sindyMeans.plotflag = 0;
+sindyMeans.diagflag = 0;
+% Create an array of SINDy objects, the array should be equal in size to the covariance of the data, so 2 dim data is 2x2 Covariance matrix, 1 SINDy Obj for each
+[heightCovMat, widthCovMat] = size(classVariances{1}); % Get size of Cov Matrix
+sindyCovariances(heightCovMat,widthCovMat) = SINDy(); % Create the array of SINDy Objects
+% Using the below synatx [objArray.property] = deal(value); will set the property of each object in objArray to value.
 [sindyCovariances.lambda] = deal(2e-6);
 [sindyCovariances.polyOrder] = deal(5);
 [sindyCovariances.useSine] = deal(0);
@@ -88,19 +85,26 @@ insert(p,int32(0),'H:\Gits\AttackingLearnPP.NSE\advlearn\advlearn\attacks\')
 % SVMAttack Parameters
 % Setup Boundary Regions
 % need to set this up for any dataset
-classNumber = 1; % class to attack
-mesh = 10.5
+step_size = 0.5;
+max_steps = 100;
+c = classNumber;
+kernel = 'rbf';
+degree = 3;
+coef0 = 1;
+gamma = 1;
+
+mesh = 10.5;
 step = 0.25;
 bound = mesh-0.5;
 boundary = [[-bound -bound];[bound bound]];
 svmPoisonAttackArgs = pyargs('boundary', boundary,...
-							 'step_size', 0.5,...
-							  'max_steps', int32(100),...
-							  'c', int32(classNumber);,...
-							  'kernel', 'rbf',...
-							  'degree', 3,...
-							   'coef0', 1,...
-							   'gamma', 1);
+							 'step_size', step_size,...
+							  'max_steps', int32(max_steps),...
+							  'c', int32(classNumber),...
+							  'kernel', kernel,...
+							  'degree', degree,...
+							   'coef0', coef0,...
+							   'gamma', gamma);
 
 
 
@@ -110,34 +114,44 @@ f_measure = zeros(n_timestamps, net.mclass);
 g_mean = zeros(n_timestamps, 1);
 recall = zeros(n_timestamps, net.mclass);
 precision = zeros(n_timestamps, net.mclass);
-err_nse = zeros(n_timestamps, 1);
-for ell = 1:n_timestamps
-	if ell == 1 % Wait two time steps before making preditions with SINDy and attacking
+errs_nse = zeros(n_timestamps, 1);
+for iTStep = 1:numTimeSteps
+	if iTStep == 1 % Wait two time steps before making preditions with SINDy and attacking
 		[~,...
-		f_measure(ell,:),...
-		g_mean(ell),...
-		precision(ell,:),...
-		recall(ell,:),...
-		errs_nse(ell)] = learn_nse_for_attacking(net,...
-												 data_train{ell},...
-												 labels_train{ell},...
-												 data_test{ell},...
-		                                         labels_test{ell});
+		f_measure(iTStep,:),...
+		g_mean(iTStep),...
+		precision(iTStep,:),...
+		recall(iTStep,:),...
+		errs_nse(iTStep)] = learn_nse_for_attacking(net,...
+												 data_train{iTStep},...
+												 labels_train{iTStep},...
+												 data_test{iTStep},...
+		                                         labels_test{iTStep});
 	else 
-		meansData = cell2mat({[classMeans{classNumber}{1,1:n_timestamps}]});
-		meansData = reshape(meansData',n_timestamps,numDims)';
-		sindyMeans(classNumber).buildModel(meansData,1,1,n_timestamps,1);
+		meansData = cell2mat({[classMeans{1:iTStep}]});
+		meansData = reshape(meansData',n_timestamps,numDims)'; % Reshape into timeStep By numDims matrix
+		sindyMeans.buildModel(meansData,1,1,iTStep,1); % data,dt,startTime,endTime,numTimeStepsToPredict,<optionally derivatives>
+		covarianceData = cell2mat({[classVariances{1:iTStep}]})';
+		covIdx = zeros(heightCovMat*iTStep,widthCovMat*iTStep);
+		horzCovLinIdx = 1:widthCovMat;
+		idxOfNxtRow = horzCovLinIdx(end)+1;	
+		secRowCovLinIdx = idxOfNxtRow:widthCovMat+idxOfNxtRow-1;
+		covRowIdxDiff = secRowCovLinIdx - horzCovLinIdx;
+		idxIncrements = (covRowIdxDiff:covRowIdxDiff:covRowIdxDiff*heightCovMat*iTStep)';
+        covLinIdx = repmat(horzCovLinIdx,iTStep*heightCovMat,1);
+        covLinIdx = bsxfun(@add,covLinIdx,idxIncrements);
+
 		% need to do sindyModels for covariances, and then generate attacks
 		[attackPoints,attackLabels] = chrisAttacks(data,labels,boundary,svmPoisonAttackArgs,numberAttackPoints);
 		[~,...
-		f_measure(ell,:),...
-		g_mean(ell),...
-		precision(ell,:),...
-		recall(ell,:),...
-		errs_nse(ell)] = learn_nse_for_attacking(net,...
-												 data_train{ell},...
-												 labels_train{ell},...
-												 data_test{ell},...
-		                                         labels_test{ell});
-
+		f_measure(iTStep,:),...
+		g_mean(iTStep),...
+		precision(iTStep,:),...
+		recall(iTStep,:),...
+		errs_nse(iTStep)] = learn_nse_for_attacking(net,...
+												 data_train{iTStep},...
+												 labels_train{iTStep},...
+												 data_test{iTStep},...
+		                                         labels_test{iTStep});
+    end
 end
