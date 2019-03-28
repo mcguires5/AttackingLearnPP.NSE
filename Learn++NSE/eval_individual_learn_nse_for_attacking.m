2fa9bcf36b2a4f7119477f0a0429ffe48512b063
mcguires5 <mcguires5@students.rowan.edu>
2019-03-28 15:15:55 -0400

    Fixed plotting of SVM Boundrys

--- Learn++NSE/eval_individual_learn_nse_for_attacking.m
+++ Learn++NSE/eval_individual_learn_nse_for_attacking.m
@@ -1,4 +1,4 @@
-function [net,f_measure,g_mean,precision,recall,err,latestClassifier] = learn_nse_for_attacking(net, data_train, labels_train, data_test, ...
+function [net,f_measure,g_mean,precision,recall,err] = eval_individual_learn_nse_for_attacking(net, data_train, labels_train, data_test, ...
   labels_test, smote_params)
 %    [net,f_measure,g_mean,precision,recall,err] = learn_nse(net, ...
 %        data_train, labels_train, ...
@@ -58,6 +58,26 @@ function [net,f_measure,g_mean,precision,recall,err,latestClassifier] = learn_ns
 %     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
+if nargin < 5
+  error('LEARN_NSE :: You need to specify all the required inputs. ')
+end
+if nargin == 5
+  smote_params = {};
+  smote_on =false;
+  net.type = 'learn++.nse';
+end
+if nargin == 6
+  smote_on = true;
+  net.type = 'learn++.cds';
+end
+
+
+net.classifiers = {};   % classifiers
+net.w = [];             % weights 
+net.initialized = false;% set to false
+net.t = 1;              % track the time of learning
+net.classifierweigths = {};               % array of classifier weights
+
 
   
   % get the training data for the 't'th round 
@@ -66,21 +86,34 @@ function [net,f_measure,g_mean,precision,recall,err,latestClassifier] = learn_ns
   data_test_t = data_test;
   labels_test_t = labels_test;
   
-
+  if smote_on == true 
+    % add learn++.cds functionality here
+    syn_data = smote(...
+      data_train_t(labels_train_t == smote_params.minority_class,:), ...
+      smote_params.k, ...
+      smote_params.N);
+    data_train_t = [data_train_t; syn_data];
+    labels_train_t = [labels_train_t;...
+      ones(size(syn_data,1),1) * smote_params.minority_class];
+    i = randperm(numel(labels_train_t));
+    labels_train_t = labels_train_t(i);
+    data_train_t = data_train_t(i, :);
+  end
+  
   % has the 
-  if net.initialized == false
+  if net.initialized == false,
     net.beta = [];
   end
   
   mt = size(data_train_t,1); % numnber of training examples
   Dt = ones(mt,1)/mt;         % initialize instance weight distribution
   
-  if net.initialized==1
+  if net.initialized==1,
     % STEP 1: Compute error of the existing ensemble on new data
     predictions = classify_ensemble(net, data_train_t, labels_train_t);
     Et = sum((predictions~=labels_train_t)/mt);
     Bt = Et/(1-Et);           % this is suggested in Metin's IEEE Paper
-    if Bt==0, Bt = 1/mt; end % clip 
+    if Bt==0, Bt = 1/mt; end; % clip 
     
     % update and normalize the instance weights
     Dt(predictions==labels_train_t) = Dt(predictions==labels_train_t) * Bt;
@@ -92,53 +125,22 @@ function [net,f_measure,g_mean,precision,recall,err,latestClassifier] = learn_ns
     net.base_classifier, ...
     data_train_t, ...
     labels_train_t);
-  
-  % STEP 4: Evaluate all existing classifiers on new data
-  t = size(net.classifiers,2);
-  y = decision_ensemble(net, data_train_t, labels_train_t, t);
 
-  for k = 1:net.t
-    epsilon_tk  = sum(Dt(y(:, k) ~= labels_train_t));
-    
-    if (k<net.t)&&(epsilon_tk>0.5) 
-      epsilon_tk = 0.5;
-    elseif (k==net.t)&&(epsilon_tk>0.5)
-      % try generate a new classifier 
-      net.classifiers{k} = classifier_train(...
-        net.base_classifier, ...  
-        data_train_t, ...
-        labels_train_t);
-      epsilon_tk  = sum(Dt(y(:, k) ~= labels_train_t));
-      epsilon_tk(epsilon_tk > 0.5) = 0.5;   % we tried; clip the loss 
-    end
-    net.beta(net.t,k) = epsilon_tk / (1-epsilon_tk);
-  end
-  
-  % compute the classifier weights
-  if net.t==1
-    if net.beta(net.t,net.t)<net.threshold
-      net.beta(net.t,net.t) = net.threshold;
-    end
-    net.w(net.t,net.t) = log(1/net.beta(net.t,net.t));
-  else
-    for k = 1:net.t
-      b = t - k - net.b;
-      omega = 1:(net.t - k + 1); 
-      omega = 1./(1+exp(-net.a*(omega-b)));
-      omega = (omega/sum(omega))';
-      beta_hat = sum(omega.*(net.beta(k:net.t,k)));
-      if beta_hat < net.threshold
-        beta_hat = net.threshold;
-      end
-      net.w(net.t,k) = log(1/beta_hat);
-    end
-  end
-  
-  % STEP 7: classifier voting weights
-  net.classifierweigths{end+1} = net.w(end,:);
-  
-  
-  [predictions,posterior,latestClassifier] = classify_ensemble(net, data_test_t, labels_test_t);
+n_experts = 1;
+p = zeros(numel(labels_test_t), net.mclass);
+
+y = classifier_test(net.classifiers{end}, data_test_t);
+
+% this is inefficient, but it does the job 
+for m = 1:numel(y)
+p(m,y(m)) = p(m,y(m));
+end
+
+[~,predictions] = max(p');
+predictions = predictions';
+sum = predictions - labels_test_t;
+posterior = p./repmat(sum(p,2),1,net.mclass);
+
   %errs(ell) = sum(predictions ~= labels_test_t)/numel(labels_test_t);
   
   [f_measure,g_mean,recall,precision,...
@@ -160,21 +162,17 @@ for k = 1:n_experts
   y(:, k) = classifier_test(net.classifiers{k}, data);
 end
 
-function [predictions,posterior,y] = classify_ensemble(net, data, labels)
-n_experts = length(net.classifiers);
-weights = net.w(end,:);
-if n_experts ~= length(weights)
-  error('What are there are different number of weights and experts!')
-end
+function [predictions,posterior] = classify_ensemble(net, data, labels)
+n_experts = 1;
 p = zeros(numel(labels), net.mclass);
-for k = 1:n_experts
-  y = classifier_test(net.classifiers{k}, data);
-  
-  % this is inefficient, but it does the job 
-  for m = 1:numel(y)
-    p(m,y(m)) = p(m,y(m)) + weights(k);
-  end
+
+y = classifier_test(net.classifiers{end}, data);
+
+% this is inefficient, but it does the job 
+for m = 1:numel(y)
+p(m,y(m)) = p(m,y(m));
 end
+
 [~,predictions] = max(p');
 predictions = predictions';
 posterior = p./repmat(sum(p,2),1,net.mclass);
